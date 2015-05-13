dccee0985bd530a6b25f8c506df9d5654c9a903d
chalam <dim2dip@gmail.com>
2015-05-12 23:29:09 -0400

    added spring boot web security with angularjs

--- src/main/java/com/chalam/GreetingController.java
+++ src/main/java/com/chalam/GreetingController.java
@@ -4,7 +4,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicLong;
+
+import java.security.Principal;
 
 /**
  * create the resource controller that will serve these greetings
@@ -16,8 +21,22 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                String.format(template, name));
     }
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World, from AngularJS");
+        return model;
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
 }
